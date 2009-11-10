49169bfd0e9da5ecdc1cffb05ab3e59067c89bfd
U-sugawara\taiki <taiki@sugawara.(none)>
2009-11-10 22:55:32 +0900

    bbdb support

--- auto-complete-config.el
+++ auto-complete-config.el
@@ -419,5 +419,41 @@
     (prefix . c-dot)
     (volatile)))
 
+
+
+;; BBDB sources
+(defvar ac-bbdb-header-list '("to" "from" "cc" "bcc"))
+
+(defun ac-bbdb-candidate ()
+  (delete-dups
+   (apply
+    'append
+    (mapcar (lambda (rec)
+	      (mapcar (lambda (n) (bbdb-dwim-net-address rec n))
+		      (bbdb-record-net rec)))
+	    (bbdb-records)))))
+
+(defun ac-bbdb-prefix ()
+  (interactive)
+  (let ((case-fold-search t))
+    (when (and
+	   (< (point)
+	      (save-excursion
+		(goto-char (point-min))
+		(search-forward (concat "\n" mail-header-separator "\n") nil t)
+		(point)))
+	   (save-excursion
+	     (beginning-of-line)
+	     (while (and (looking-at "^[ \t]")
+			 (not (= (point) (point-min))))
+	       (forward-line -1))
+	     (looking-at (concat (regexp-opt ac-bbdb-header-list t) ":"))))
+      (ac-prefix-symbol))))
+
+(defvar ac-source-bbdb
+  '((candidates . ac-bbdb-candidate)
+    (match . substring)
+    (prefix . ac-bbdb-prefix)))
+
 (provide 'auto-complete-config)
 ;;; auto-complete-config.el ends here
